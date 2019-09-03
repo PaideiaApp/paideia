9639df27c0d1d11de13e5d1dff499404d202f0cb
Arthur James <11641990+ArtiDjeims@users.noreply.github.com>
2019-09-03 18:30:01 +0300

    weird request redirect on SW

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
-'use strict';
+// 'use strict';
 
-var cacheVersion = 3;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = '/offline.html';
+// var cacheVersion = 3;
+// var currentCache = {
+//   offline: 'offline-cache' + cacheVersion
+// };
+// const offlineUrl = '/offline.html';
 
-this.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-          offlineUrl
-      ]);
-    }) 
-  );
-});
+// this.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(currentCache.offline).then(function(cache) {
+//       return cache.addAll([
+//           offlineUrl
+//       ]);
+//     }) 
+//   );
+// });
 
-this.addEventListener('fetch', event => {
-    // request.mode = navigate isn't supported in all browsers
-    request.redirect = 'follow';
-    // so include a check for Accept: text/html header.
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-          event.respondWith(
-            fetch(event.request.url).catch(error => {
-                // Return the offline page
-                return caches.match(offlineUrl);
-            })
-      );
-    }
-    else{
-          // Respond with everything else if we can
-          event.respondWith(caches.match(event.request)
-                          .then(function (response) {
-                          return response || fetch(event.request);
-                      })
-              );
-            //  // Return the offline page
-            //  return caches.match(offlineUrl);
-        }
-  });
+// this.addEventListener('fetch', event => {
+//     // request.mode = navigate isn't supported in all browsers
+//     // event.request.redirect = 'follow';
+//     // so include a check for Accept: text/html header.
+//     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+//           event.respondWith(
+//             fetch(event.request.url).catch(error => {
+//                 // Return the offline page
+//                 return caches.match(offlineUrl);
+//             })
+//       );
+//     }
+//     else{
+//           // Respond with everything else if we can
+//           event.respondWith(caches.match(event.request)
+//                           .then(function (response) {
+//                           return response || fetch(event.request);
+//                       })
+//               );
+//             //  // Return the offline page
+//             //  return caches.match(offlineUrl);
+//         }
+//   });
 
 
 
