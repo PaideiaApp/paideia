d6d5400fdcf208e3ea80e342ec0b93f57bbda62f
Arthur James <11641990+ArtiDjeims@users.noreply.github.com>
2019-11-20 15:29:07 +0200

    pwa promt 6

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 
-var version = `v1.0.1.${new Date().getDate()}${new Date().getHours()}::`;
+var version = `v1${new Date().getDate()}${new Date().getHours()}::`;
 
 self.addEventListener("install", function(event) {
   console.log('WORKER: install event in progress.');
@@ -20,9 +20,8 @@ self.addEventListener("install", function(event) {
         */
         return cache.addAll([
           '/',
-          '/app/',
-          '/dashboard/',
-          '../assets/',
+          '/dashboard',
+          '/assets/front/css/install.css',
         ]);
       })
       .then(function() {
